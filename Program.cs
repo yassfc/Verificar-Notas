7fb432efeaf715ac1a6cb592599fbedca6238673
ㅤ <141974926+yassfc@users.noreply.github.com>
2025-02-18 02:21:00 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,35 +3,46 @@
     {
         static void Main()
         {       
-            Inicio:
+            I:
             Console.Clear(); 
             string pergunta;
             int teste;
-            int num;  
-            Console.WriteLine("Qual foi sua nota?");
+            int num, num2, num3, num4;
+              
+            Console.WriteLine("Qual foi sua nota? (1 bimestre)");
             num=int.Parse(Console.ReadLine());
-        
-            string r = "Aprovado";
-            if (num >=60){
-            }else
-              r="Reprovado";
-            Console.WriteLine("Voce foi {0}", r);
-    
-            {
+            
+            Console.WriteLine("Qual foi sua nota? (2 bimestre)");
+            num2=int.Parse(Console.ReadLine());
+          
+            Console.WriteLine("Qual foi sua nota? (3 bimestre)");
+            num3=int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual foi sua nota? (4 bimestre)");
+            num4=int.Parse(Console.ReadLine());
+
+            Console.Clear();
+            int soma = num + num2 + num3 + num4;
+
+                string r = "Aprovado";
+                if (soma >=60){
+                }else
+                r="Reprovado";
+                Console.WriteLine("Nota final: {0} | Voce foi {1}",soma, r); 
+               {
+
                //BEM AQUI!!!!!!!!!!!!!!
-                 
-        Console.WriteLine("Deseja recomeçar?");
+
+                Console.WriteLine(" ");
+                Console.WriteLine("Deseja recomeçar? (Sim/Não)");
                 pergunta = Console.ReadLine();
 
-                if(pergunta == "Sim"){
-                    goto Inicio;
-
-                }else{
-
+                    if(pergunta == "Sim"){
+                    goto I;
+                    }else{
                      Console.ReadLine();
+
                 }
-
-
             }
         } 
      
