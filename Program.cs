827318ac010377a7c839e1b45befa80274584b55
ㅤ <141974926+yassfc@users.noreply.github.com>
2025-02-18 01:39:26 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+    class Program
+    {
+        static void Main()
+        {       
+            Inicio:
+            Console.Clear(); 
+            string pergunta;
+            int teste;
+            int num;  
+            Console.WriteLine("Qual foi sua nota?");
+            num=int.Parse(Console.ReadLine());
+        
+            string r = "Aprovado";
+            if (num >=60){
+            }else
+              r="Reprovado";
+            Console.WriteLine("Voce foi {0}", r);
+    
+            {
+               //BEM AQUI!!!!!!!!!!!!!!
+                 
+        Console.WriteLine("Deseja recomeçar?");
+                pergunta = Console.ReadLine();
+
+                if(pergunta == "Sim"){
+                    goto Inicio;
+
+                }else{
+
+                     Console.ReadLine();
+                }
+
+
+            }
+        } 
+     
+}
+    
